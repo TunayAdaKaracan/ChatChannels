0e115f82f6b8a65368acb699179d5cf511468860
Tunay Ada <tunayada@gmail.com>
2022-02-23 19:16:39 +0300

    Initial commit

---
+++ src/main/java/dev/kutuptilkisi/chatchannels/Chatchannels.java
@@ -0,0 +1,12 @@
+package dev.kutuptilkisi.chatchannels;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Chatchannels extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+}
